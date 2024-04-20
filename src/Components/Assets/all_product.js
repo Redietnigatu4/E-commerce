f2065c8a698f5da7fd314f8f61510d79a96cf2e2
Redietnigatu4 <aboyeredu@gmail.com>
2024-04-20 16:39:04 +0300

    first commit

---
+++ src/Components/Assets/all_product.js
@@ -0,0 +1,218 @@
+import p1_img from './dress1.webp'
+import p2_img from './dress2.jpg'
+import p3_img from './dress3.webp'
+import p4_img from './dress4.avif'
+import p5_img from './dress5.jpb'
+import p6_img from './dress6.webp'
+import p7_img from './dress7.webp'
+import p8_img from './dress8.jpg'
+import p9_img from './dress9.webp'
+import p10_img from './dress10.jpg'
+import p11_img from './dress11.jpg'
+
+import p12_img from './men1.webp'
+import p13_img from './men2.jpg'
+import p14_img from './men3.jpg'
+import p15_img from './men4.jpg'
+import p16_img from './men5.jpg'
+import p17_img from './men6.jpg'
+import p18_img from './men7.jpg'
+import p19_img from './men8.webp'
+import p20_img from './men9.jpg'
+import p21_img from './men10.jpg'
+
+
+let all_produduct = [
+    {
+        id : 1,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p1_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 2,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p2_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 3,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p3_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 4,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p4_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 5,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p5_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 6,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p6_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 7,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p7_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 8,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p8_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 9,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p9_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 10,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p10_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 11,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p11_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 12,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p12_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 13,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p13_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 14,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p14_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 15,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p15_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 16,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p16_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 17,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p17_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 18,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p18_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 19,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p19_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 20,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p20_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 21,
+        name: "striped Flutter ",
+        category:"Men",
+        image:p21_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    
+
+
+]
---
+++ src/Components/Assets/data.js
@@ -0,0 +1,45 @@
+import p1_img from './dress1.webp'
+import p2_img from './dress2.jpg'
+import p3_img from './dress3.webp'
+import p4_img from './dress4.avif'
+ 
+let data_product = [
+    {
+        id : 1,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p1_img,
+        
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 2,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p2_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 3,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p3_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+    {
+        id : 4,
+        name: "striped Flutter ",
+        category:"Women",
+        image:p4_img,
+        new_price: 50.0,
+        old_price: 80.5,
+
+    },
+]
+export default data_product
